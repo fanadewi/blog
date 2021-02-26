e165fdf60d2f16f997a2609dcb2773b926928232
Ana <f.ana.d@outlook.co.id>
2021-02-26 19:23:35 +0700

    fix post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
     def is_owner?(post)
-        user_signed_in? && current_user == post.user
+        user_signed_in? && (current_user == post.user || current_user.is_admin?)
     end
 end
